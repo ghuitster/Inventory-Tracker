d6ca3f6e813549ba8f72e853418e23ebe18a4c7c
Michael Eagar <meagar@proofpoint.com>
2013-09-21 11:17:11 -0600

    Added model, model.serializer, and model.report packages. Adde UnitType and UnitSize

---
+++ inventory-tracker/src/model/UnitSize.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+package model;
+
+/**
+ * @author Michael
+ *
+ */
+public class UnitSize
+{
+	// Variables
+	private float size = 0.0f;
+	private UnitType unit = null;
+	
+	// Constructor
+	public UnitSize(float s, UnitType u)
+	{
+		this.size = s;
+		this.unit = u;
+	}
+	
+	// Method
+	
+	
+	
+}
---
+++ inventory-tracker/src/model/UnitType.java
@@ -0,0 +1,13 @@
+/**
+ * 
+ */
+package model;
+
+/**
+ * A defined set of units for defining unit size and three month supply in the Inventory Tracker.
+ * @author Michael
+ */
+public enum UnitType
+{
+	Count, Pounds, Ounces, Grams, Kilograms, Gallons, Quarts, Pints, Fluid_Ounces, Liters, Elephant_Weight, Stone, Chevrolet
+}
