3a53e2e7f535bf21b25ecdd6632463ef321f030e
Michael Eagar <meagar@proofpoint.com>
2013-09-21 12:56:48 -0600

    Stubbed out Barcode.

--- inventory-tracker/src/model/Barcode.java
+++ inventory-tracker/src/model/Barcode.java
@@ -2,10 +2,102 @@
 package model;
 
 /**
- * @author Brian
+ * Class to represent the Barcode for an Item
+ * @author Michael
  * 
  */
 public class Barcode
 {
+	// Variables
+	private String number;
+
+	/**
+	 * The Barcode constructor
+	 * @param number the unique number that will identify this Barcode
+	 */
+	public Barcode(String number)
+	{
+		this.number = number;
+	}
+
+	// Methods
+	/**
+	 * @return the number
+	 */
+	public String getNumber()
+	{
+		return number;
+	}
+
+	/**
+	 * @param number the number to set
+	 */
+	public void setNumber(String number)
+	{
+		this.number = number;
+	}
+
+	/**
+	 * @param number the number to attempt to set
+	 * @return whether or not the number can be set
+	 */
+	public boolean ableToSetNumber(String number)
+	{
+		return true;
+	}
+
+	/**
+	 * @see java.lang.Object#hashCode()
+	 */
+	@Override
+	public int hashCode()
+	{
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((number == null) ? 0 : number.hashCode());
+		return result;
+	}
+
+	/**
+	 * @see java.lang.Object#equals(java.lang.Object)
+	 */
+	@Override
+	public boolean equals(Object obj)
+	{
+		if(this == obj)
+		{
+			return true;
+		}
+		if(obj == null)
+		{
+			return false;
+		}
+		if(getClass() != obj.getClass())
+		{
+			return false;
+		}
+		Barcode other = (Barcode) obj;
+		if(number == null)
+		{
+			if(other.number != null)
+			{
+				return false;
+			}
+		}
+		else if(!number.equals(other.number))
+		{
+			return false;
+		}
+		return true;
+	}
+
+	/**
+	 * @see java.lang.Object#toString()
+	 */
+	@Override
+	public String toString()
+	{
+		return "Barcode [number=" + number + "]";
+	}
 
 }
