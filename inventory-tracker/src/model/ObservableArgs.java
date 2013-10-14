19c5f54d51cff82064842dc3ac4e3fe836eb6a3f
Brian Spencer <BrianSpencer@outlook.com>
2013-10-13 20:28:07 -0600

    Changed observables to use class rather than enum

---
+++ inventory-tracker/src/model/ObservableArgs.java
@@ -0,0 +1,35 @@
+package model;
+
+public class ObservableArgs
+{
+	private Object changedObj;
+	private UpdateType updateType;
+	
+	public ObservableArgs(Object changedObj, UpdateType updateType)
+	{
+		this.changedObj = changedObj;
+		this.updateType = updateType;
+	}
+	
+	/**
+	 * Gets the object which changed
+	 * @pre none
+	 * @post none
+	 * @return The changed object
+	 */
+	public Object getChangedObj()
+	{
+		return this.changedObj;
+	}
+	
+	/**
+	 * Gets what action occured with the changed object
+	 * @pre none
+	 * @post none
+	 * @return An enumeration representing the update type
+	 */
+	public UpdateType getUpdateType()
+	{
+		return this.updateType;
+	}
+}
--- inventory-tracker/src/observer/NSA.java
+++ inventory-tracker/src/observer/NSA.java
@@ -28,6 +28,8 @@ public class NSA implements Observer
 	 * Receiving function for then an observable in the system changes
 	 * @param o The Observable that changed
 	 * @param arg Type object for the interface, but is expected to be enumeration type
+	 * @pre arg is of type model.ObservableArg. o and arg are not null. 
+	 * @post The change described in arg has been reported to the controller, to be refreshed in the view
 	 * UpdateType. Described what the change was to the Observable
 	 */
 	@Override
