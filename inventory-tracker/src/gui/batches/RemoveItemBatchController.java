361cec681b012579e4d36d94f340f072642aed5b
David Barley <ghuitster@gmail.com>
2013-10-19 05:56:46 -0600

    One more small change to remove item batch.

--- inventory-tracker/src/gui/batches/RemoveItemBatchController.java
+++ inventory-tracker/src/gui/batches/RemoveItemBatchController.java
@@ -149,6 +149,9 @@ public class RemoveItemBatchController extends Controller implements
 		if(removingItem == null)
 		{
 			getView().displayErrorMessage("No item with that barcode");
+			barcode = "";
+			enableComponents();
+			loadValues();
 			return;
 		}
 
