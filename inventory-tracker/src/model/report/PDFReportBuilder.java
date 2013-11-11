1e9e88009f74b03ad7ae6e6368dd613a156fc86b
Michael Eagar <meagar@proofpoint.com>
2013-11-11 12:24:23 -0700

    Added Apache commons.lang for StringEscapeUtils.  Also changed PDFReportBuilder to inherit from HTMLReportBuilder.

--- inventory-tracker/src/model/report/PDFReportBuilder.java
+++ inventory-tracker/src/model/report/PDFReportBuilder.java
@@ -1,94 +1,27 @@
 /**
  * 
  */
+
 package model.report;
 
 /**
  * @author Michael
- *
+ * 
  */
-public class PDFReportBuilder implements IReportBuilder
+public class PDFReportBuilder extends HTMLReportBuilder
 {
 
 	/**
-	 * 
+	 * A class to create a PDF version of a report and save it as a PDF file.
 	 */
 	public PDFReportBuilder()
 	{
 		// TODO Auto-generated constructor stub
 	}
 
-	/* (non-Javadoc)
-	 * @see model.report.IReportBuilder#buildHead(java.lang.String)
-	 */
-	@Override
-	public void buildHead(String title)
-	{
-		// TODO Auto-generated method stub
-
-	}
-
-	/* (non-Javadoc)
-	 * @see model.report.IReportBuilder#addSectionHeader(java.lang.String)
-	 */
-	@Override
-	public void addSectionHeader(String title)
-	{
-		// TODO Auto-generated method stub
-
-	}
-
-	/* (non-Javadoc)
-	 * @see model.report.IReportBuilder#addText(java.lang.String)
-	 */
-	@Override
-	public void addText(String text)
-	{
-		// TODO Auto-generated method stub
-
-	}
-
-	/* (non-Javadoc)
-	 * @see model.report.IReportBuilder#startTable(java.lang.String[])
-	 */
-	@Override
-	public void startTable(String[] columnHeaders)
-	{
-		// TODO Auto-generated method stub
-
-	}
-
-	/* (non-Javadoc)
-	 * @see model.report.IReportBuilder#addTableRow(java.lang.String[])
-	 */
-	@Override
-	public void addTableRow(String[] cells)
-	{
-		// TODO Auto-generated method stub
-
-	}
-
-	/* (non-Javadoc)
-	 * @see model.report.IReportBuilder#addBulletedList(java.lang.String[])
-	 */
-	@Override
-	public void addBulletedList(String[] items)
-	{
-		// TODO Auto-generated method stub
-
-	}
-
-	/* (non-Javadoc)
-	 * @see model.report.IReportBuilder#finishTable()
-	 */
-	@Override
-	public void finishTable()
-	{
-		// TODO Auto-generated method stub
-
-	}
-
-	/* (non-Javadoc)
+	/*
+	 * (non-Javadoc)
+	 * 
 	 * @see model.report.IReportBuilder#finishAndSave(java.lang.String)
 	 */
 	@Override
