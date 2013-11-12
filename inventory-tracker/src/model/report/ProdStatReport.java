df50b76b2edb29ef1f1e9d7581a5c3a47c3bed7b
David Barley <ghuitster@gmail.com>
2013-11-11 23:08:22 -0700

    Finished the product statistics report.

--- inventory-tracker/src/model/report/ProdStatReport.java
+++ inventory-tracker/src/model/report/ProdStatReport.java
@@ -1,22 +1,27 @@
 
 package model.report;
 
+import java.io.File;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.List;
 
 import model.ProductStats;
 
 public class ProdStatReport extends Report
 {
+	private final int monthsToShow;
+
 	// Variables
 	private final List<ProductStats> productStats;
 
 	// Constructor
 	public ProdStatReport(List<ProductStats> productStats,
-			IReportBuilder builder)
+			IReportBuilder builder, int monthsToShow)
 	{
 		super(builder);
 		this.productStats = productStats;
-		// TODO Auto-generated constructor stub
+		this.monthsToShow = monthsToShow;
 	}
 
 	// Methods
@@ -25,14 +30,59 @@ public class ProdStatReport extends Report
 	 */
 	public List<ProductStats> getProductStats()
 	{
-		// TODO
 		throw new UnsupportedOperationException();
 	}
 
 	@Override
 	public void createReport()
 	{
-		// TODO Auto-generated method stub
+		builder.buildHead("Product Report (" + monthsToShow + " Months)");
+		String[] columns =
+				{"Description", "Barcode", "Size", "3-Month Supply",
+						"Supply: Cur/Avg", "Supply: Min/Max",
+						"Supply: Used/Added", "Shelf Life",
+						"Used Age: Avg/Max", "Cur Age: Avg/Max"};
 
+		builder.startTable(columns);
+
+		for(ProductStats stats: productStats)
+			builder.addTableRow(createRow(stats));
+
+		builder.finishTable();
+		String timeStamp =
+				new SimpleDateFormat("yyyyMMdd_HHmmss").format(Calendar
+						.getInstance().getTime());
+		String filename =
+				"Reports" + File.separator + "ProductStatisticsReport-"
+						+ timeStamp + ".pdf";
+
+		this.builder.finishAndSave(filename);
+	}
+
+	private String[] createRow(ProductStats stats)
+	{
+		String desc = stats.getProduct().getDescription().getDescription();
+		String barcode = stats.getProduct().getBarcode().getNumber();
+		String size = stats.getProduct().getSize().toString();
+		String supply = stats.getProduct().getThreeMonthSupply().toString();
+		String supplyCurAndAverage =
+				stats.getCurrentSupply() + " / " + stats.getAverageSupply();
+		String supplyMinAndMax =
+				stats.getMinSupply() + " / " + stats.getMaxSupply();
+		String supplyUsedAndAdded =
+				stats.getUsedSupply() + " / " + stats.getAddedSupply();
+		String shelfLife = stats.getProduct().getShelfLife() + " months";
+		String usedAgeAvgAndMax =
+				stats.getAvgUsedAge() + " days / " + stats.getMaxUsedAge()
+						+ " days";
+		String curAgeAvgAndMax =
+				stats.getAvgCurrentAge() + " days / "
+						+ stats.getMaxCurrentAge() + " days";
+
+		String[] row =
+				{desc, barcode, size, supply, supplyCurAndAverage,
+						supplyMinAndMax, supplyUsedAndAdded, shelfLife,
+						shelfLife, usedAgeAvgAndMax, curAgeAvgAndMax};
+		return row;
 	}
 }
