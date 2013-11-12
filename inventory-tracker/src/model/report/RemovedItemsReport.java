e7121ea5d56e4232ed3c4c70f5e672196c51656f
Michael Eagar <meagar@proofpoint.com>
2013-11-11 21:23:46 -0700

    I think RemovedItemsReport.java is done.

--- inventory-tracker/src/model/report/RemovedItemsReport.java
+++ inventory-tracker/src/model/report/RemovedItemsReport.java
@@ -36,6 +36,30 @@ public class RemovedItemsReport extends Report
 	@Override
 	public void createReport(String path)
 	{
+		this.builder.buildHead("Items Removed Since " + this.dateString);
+		String[] columns =
+				{"Description", "Size", "Product Barcode", "Removed",
+						"Current Supply"};
+		this.builder.startTable(columns);
 
+		for(RemovedItems items: this.removedItems)
+		{
+			this.builder.addTableRow(this.createRow(items));
+		}
+
+		this.builder.finishTable();
+		this.builder.finishAndSave(path);
+	}
+
+	private String[] createRow(RemovedItems items)
+	{
+		String desc = items.getProduct().getDescription().getDescription();
+		String size = items.getProduct().getSize().toString();
+		String barcode = items.getProduct().getBarcode().getNumber();
+		String numRemoved = items.getCount() + "";
+		String supply = items.getSupply() + "";
+
+		String[] row = {desc, size, barcode, numRemoved, supply};
+		return row;
 	}
 }
