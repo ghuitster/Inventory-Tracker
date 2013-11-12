c900e005bf26c30973713128f98990c1731878db
Chris Thomsen <chriscout2007@yahoo.com>
2013-11-11 20:36:12 -0800

    ExpItemsReport is done, we think.

--- inventory-tracker/src/model/report/ExpItemsReport.java
+++ inventory-tracker/src/model/report/ExpItemsReport.java
@@ -3,7 +3,10 @@ package model.report;
 
 import java.util.List;
 
+import common.util.DateUtils;
+
 import model.IItem;
+import model.ProductGroup;
 
 public class ExpItemsReport extends Report
 {
@@ -21,5 +24,35 @@ public class ExpItemsReport extends Report
 	public void createReport(String path)
 	{
 		this.builder.buildHead("Expired Items");
+		String[] temp = {"Description", "Storage Unit", "Product Group", "Entry Date", "Expire Date", "Item Barcode"};
+		this.builder.startTable(temp);
+		
+		for (IItem item : this.items)
+		{
+			this.builder.addTableRow(this.createRowArray(item));
+		}
+		
+		this.builder.finishTable();
+		this.builder.finishAndSave(path);	
+	}
+	
+
+	/**
+	 * @param item
+	 */
+	private String[] createRowArray(IItem item)
+	{
+		String desc = item.getProduct().getDescription().getDescription();
+		String storageUnit = item.getContainer().getStorageUnit().getName();
+		String productGroup = "";
+		if (item.getContainer() instanceof ProductGroup)
+			productGroup = item.getContainer().getName();
+		String entry = DateUtils.formatDate(item.getEntryDate());
+		String exit = DateUtils.formatDate(item.getExpirationDate());
+		String barcode = item.getBarcode().getNumber();
+		
+		String[] row = {desc, storageUnit, productGroup, entry, exit, barcode};
+		return row;
 	}
 }
+
