eb52e2976d8cf7bcd69f324b5cd6945b25fcc61a
David Barley <ghuitster@gmail.com>
2013-12-04 22:40:37 -0700

    Added a blank plugin for Brian.

--- inventory-tracker/src/model/plugin/BrianPlugin.java
+++ inventory-tracker/src/model/plugin/BrianPlugin.java
@@ -3,12 +3,14 @@ package model.plugin;
 
 public class BrianPlugin extends Plugin
 {
-
 	@Override
 	public String findProduct(String barcode)
 	{
-		// TODO Auto-generated method stub
-		return null;
-	}
+		String productDescription = null;
 
+		if(productDescription == null && this.nextPlugin != null)
+			return this.nextPlugin.findProduct(barcode);
+		else
+			return productDescription;
+	}
 }
