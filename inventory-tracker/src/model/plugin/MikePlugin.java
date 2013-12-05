2c12de1e2e30e5aa1bcb87f55093c298b521597b
Michael Eagar <mkeagar@gmail.com>
2013-12-04 17:28:20 -0700

    Added simple json parser to buildpath.

--- inventory-tracker/src/model/plugin/MikePlugin.java
+++ inventory-tracker/src/model/plugin/MikePlugin.java
@@ -3,6 +3,12 @@ package model.plugin;
 
 public class MikePlugin extends Plugin
 {
+	// Variables
+	private final String BASE_URL = null;
+
+	// Constructor
+
+	// Methods
 
 	@Override
 	public String findProduct(String barcode)
